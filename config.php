0546ae25ef24445059749ba56b1a975233b6d43a
namcoindev <69799859+namcoindev@users.noreply.github.com>
2020-09-10 23:27:11 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-        'api' => 'http://138.68.31.205:11778/',
+        'api' => 'https://api.namcoin.org/',
         'blockTargetInterval' => 30,
         'coinUnits' => 1000000
 );
